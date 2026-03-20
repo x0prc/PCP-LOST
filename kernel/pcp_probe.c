ed122993b61d7785e33fa30a6810f5bc9e8a723b
x0prc <c0ldheat@protonmail.com>
2026-03-20 13:33:36 +0530

    feat: init probe for pcp

---
+++ kernel/pcp_probe.c
@@ -0,0 +1,8 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/mm.h>
+#include <linux/miscdevice.h>
+#include <linux/fs.h>
+#include <linux/uaccess.h>
+#include <linux/ktime.h>
+
